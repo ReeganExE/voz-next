07e40649c36a24c3b1e5750212580bd2e23a33c7
ReeganExE <dongian.rapclubkhtn@gmail.com>
2016-02-20 14:17:54 +0700

    Added  - Goto pre/next post by arrow key

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 ## Vozforums Next Page Widget
 
-Vozforums Next Page Widget by Ninh #ReeganExE
+Vozforums Next Page Widget by Ninh Pham #ReeganExE
 
 Dễ dàng qua trang với VozNext
 
-![Screen](http://pik.vn/20160f054157-0f2a-4306-9825-8d3948f5f186.png) 
+![Screen](http://pik.vn/20160f054157-0f2a-4306-9825-8d3948f5f186.png)
+
+Dùng phím mũi tên qua trái, phải để di chuyển đến comment trước hoặc kế tiếp.
+
+![Screen](https://d93c6f41d97e2eb9fe3161071406c257541cc016.googledrive.com/host/0BxLbbrIoJOa6VGpMWVRUSEVTS2c/guide.jpg)
--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 (function($) {
+    $.fn.nmap = Array.prototype.map;
     var html = ['<div id="voz-next-box" title="Voz Next Page Widget (#ReeganExE)">',
             '  <a href="#" id="z-pre-page" z-rel="prev"><img id="voz-pre" src="https://cdn3.iconfinder.com/data/icons/basic-flat-svg/512/svg10-512.png" /></a>',
             '  <a href="#" id="z-next-page" z-rel="next"><img src="https://cdn3.iconfinder.com/data/icons/basic-flat-svg/512/svg10-512.png" /></a>',
@@ -31,7 +32,68 @@
     $('a[href^="/redirect/index.php"]').each(function() {
         var href = this.href;
         this.href = decodeURIComponent(href.substr(href.indexOf('link=') + 5));
-    })
+    });
+
+
+    var posts = $('.voz-postbit a[href*="showpost"]').nmap(function(a) { return new Post(a) }).reverse();
+
+    function Post(el) {
+        this.id = el.id;
+        this.element = el;
+        this.top = $(el).closest('table').offset().top;
+    }
+
+    Post.prototype.goto = function() {
+        this.element.scrollIntoView();
+    };
+
+    Array.prototype.hasNext = function(i) {
+        return ((i + 1) < this.length)
+    };
+    
+    Array.prototype.last = function(i) {
+        return this[this.length - 1];
+    };
+
+    function prePost() {
+        var currentPosition = $(document).scrollTop();
+        for (var i = 0; i < posts.length; i++) {
+            var post = posts[i];
+            if (currentPosition > post.top) {
+                posts.hasNext(i) && posts[i + 1].goto();
+                return;
+            }
+        }
+    }
+
+    function nextPost() {
+        var currentPosition = $(document).scrollTop();
+        for (var i = 0; i < posts.length; i++) {
+            var post = posts[i];
+            if (currentPosition > post.top) {
+                i > 0 && posts[i - 1].goto();
+                return;
+            }
+        }
+        // goto the first posts
+        posts.last().goto();
+    }
+
+    $(document).keydown(function(e) {
+        var tag = e.target.tagName.toLowerCase();
+        if (tag === 'input' || tag === 'textarea') {
+            return;
+        }
+        if (e.which == 37) { // Left Arrow
+            prePost();
+        }
+
+        if (e.which == 39) { // Right Arrow
+            nextPost();
+        }
+    });
+
+
 
     function getPos() {
         var pos = localStorage.getItem('pos');
